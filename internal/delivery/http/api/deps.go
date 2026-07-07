219a57722f037507b4d6367ff1d6d913018b4f20
Nidael1 <drosasg@gmail.com>
2026-07-07 00:50:24 -0600

    [adr-0019] handlers GET admin/metrics + pool en Deps + rutas (issue #227)

--- internal/delivery/http/api/deps.go
+++ internal/delivery/http/api/deps.go
@@ -1,6 +1,8 @@
 package api
 
 import (
+	"github.com/jackc/pgx/v5/pgxpool"
+
 	"github.com/Nidael1/VuhmikGO/internal/application"
 	"github.com/Nidael1/VuhmikGO/internal/application/ports"
 	"github.com/Nidael1/VuhmikGO/internal/infrastructure/postgres"
@@ -9,22 +11,23 @@ import (
 
 // Deps contiene las dependencias inyectadas en los handlers de la API.
 type Deps struct {
+	DB               *pgxpool.Pool
 	EvidenceRepo     ports.EvidenceRepository
 	UserRepo         *postgres.UserRepository
 	PatientRepo      *postgres.PatientRepository
 	RefreshTokenRepo *postgres.RefreshTokenRepository
 	RedisClient      *infraredis.Client
 	CapabilityRepo   ports.CapabilityRepository
-	AllergyService        *application.AllergyService
-	AllergyProjectionRepo ports.AllergyProjectionRepository
-	ProfileRepo           ports.ProfileRepository
+	AllergyService             *application.AllergyService
+	AllergyProjectionRepo      ports.AllergyProjectionRepository
+	ProfileRepo                ports.ProfileRepository
 	NoteProjectionRepo         ports.NoteProjectionRepository
-	PrescriptionService       *application.PrescriptionService
-	PrescriptionProjectionRepo   ports.PrescriptionProjectionRepository
-	ConsultationService          *application.ConsultationService
-	ConsultationProjectionRepo   ports.ConsultationProjectionRepository
-	TenantRepo                   ports.TenantRepository
-	VendorRepo                   ports.VendorRepository
+	PrescriptionService        *application.PrescriptionService
+	PrescriptionProjectionRepo ports.PrescriptionProjectionRepository
+	ConsultationService        *application.ConsultationService
+	ConsultationProjectionRepo ports.ConsultationProjectionRepository
+	TenantRepo                 ports.TenantRepository
+	VendorRepo                 ports.VendorRepository
 }
 
 var deps Deps
